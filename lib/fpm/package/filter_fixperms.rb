f7a4e7f37a29357399e83b6080942076b6ac2ed7
mario <mario>
2014-12-18 16:36:57 +0000

    Umask 644/755 (mostly redundant for rpm/deb targets though; already contain permission concrete logic)

---
+++ lib/fpm/package/filter_fixperms.rb
@@ -0,0 +1,13 @@
+# Set file permissions to 644/755 maximum
+
+require "fpm/package"
+require "fpm/util"
+require "fileutils"
+
+class FPM::Package::Filter_fixperms < FPM::Package
+  def update
+    ::Dir["#{staging_path}/**/*"].each do |fn|
+      File.chmod(File.stat(fn).mode & 0777755, fn)
+    end
+  end
+end
